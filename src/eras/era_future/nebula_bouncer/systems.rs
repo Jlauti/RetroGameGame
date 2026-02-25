5496fca5fe1cdee98cefa5ef1e0a93d3008df350
Jlautiainen <jlautiainen@gmail.com>
2026-02-26 00:32:55 +0200

    NB-A2-010: simplified model rotation to FRAC_PI_2 for correct top-up orientation, reset facing constant

--- src/eras/era_future/nebula_bouncer/systems.rs
+++ src/eras/era_future/nebula_bouncer/systems.rs
@@ -28,7 +28,7 @@ const PREFLIGHT_SUMMARY_REL_PATH: &str =
 const FEEDBACK_TELEMETRY_COOLDOWN_SECS: f32 = 0.35;
 const MODEL_UNIT_TO_WORLD: f32 = 120.0;
 const PLAYER_MODEL_VISUAL_LIFT: f32 = 42.0;
-const PLAYER_MODEL_FACING_FIX_RADIANS: f32 = std::f32::consts::PI;
+const PLAYER_MODEL_FACING_FIX_RADIANS: f32 = 0.0;
 const SCOUT_SPRITE_SIZE: Vec2 = Vec2::new(62.0, 62.0);
 const HEAVY_SPRITE_SIZE: Vec2 = Vec2::new(78.0, 78.0);
 const INTERCEPTOR_SPRITE_SIZE: Vec2 = Vec2::new(70.0, 70.0);
@@ -418,8 +418,7 @@ pub fn setup_nebula_bouncer(
                 SceneRoot(asset_server.load(asset_manifest.player_ship.clone())),
                 Transform::from_translation(Vec3::new(0.0, 0.0, PLAYER_MODEL_VISUAL_LIFT))
                     .with_rotation(
-                        Quat::from_rotation_z(std::f32::consts::PI)
-                            * Quat::from_rotation_x(std::f32::consts::FRAC_PI_2)
+                        Quat::from_rotation_x(std::f32::consts::FRAC_PI_2)
                             * Quat::from_rotation_z(PLAYER_MODEL_FACING_FIX_RADIANS),
                     )
                     .with_scale(Vec3::splat(MODEL_UNIT_TO_WORLD)),
@@ -1170,10 +1169,9 @@ pub fn spawn_next_chunk(
                             SceneRoot(
                                 asset_server.load(asset_manifest.enemy_model_default.clone()),
                             ),
-                            Transform::from_rotation(
-                                Quat::from_rotation_z(std::f32::consts::PI)
-                                    * Quat::from_rotation_x(std::f32::consts::FRAC_PI_2),
-                            )
+                            Transform::from_rotation(Quat::from_rotation_x(
+                                std::f32::consts::FRAC_PI_2,
+                            ))
                             .with_scale(Vec3::splat(model_scale)),
                         ));
                     });
